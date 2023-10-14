4498d327f02579b3d1254bbb9654cbca3c7520b2
francescomerighi1202 <francescomerighi61@gmail.com>
2023-10-14 23:22:51 +0200

    End project

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let todosPrinc = [];
+let todosWork = [];
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { todosPrinc: todosPrinc });
+});
+
+app.get('/work', (req, res) => {
+    res.render('work.ejs', { todosWork: todosWork });
+});
+
+app.post('/create', (req, res) => {
+    todosPrinc.push(req.body.todoPrinc);
+    res.redirect('/');
+});
+
+app.post('/work/create', (req, res) => {
+    todosWork.push(req.body.todoWork);
+    res.redirect('/work');
+});
+
+app.get('/delete-all', (req, res) => {
+    todosPrinc = [];
+    res.redirect('/');
+});
+
+app.get('/work/delete-all', (req, res) => {
+    todosWork = [];
+    res.redirect('/work');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
