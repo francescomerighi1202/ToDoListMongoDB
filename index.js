70ba3819a6c5dda58a77246b2786c1e2663efacd
francescomerighi1202 <francescomerighi61@gmail.com>
2023-10-29 10:36:02 +0100

    DB and user sessions implemented

--- index.js
+++ index.js
@@ -1,113 +1,307 @@
 import express from 'express';
 import session from 'express-session';
+import flash from 'express-flash';
+import mongoose, { mongo } from 'mongoose';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const URI = process.env.MONGODB_URI || 'mongodb+srv://emmeelite:admin69@clustertest.jfiko18.mongodb.net/todolistDB?retryWrites=true&w=majority';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(flash());
 
-// Session middleware
-// DISCLAIMER: Questo NON è assolutamente un modo sicuro per salvare le sessioni utente in produzione.
-// L'ho utilizzato solamente per scopo didattico.
-// Al posto di questo, si dovrebbe utilizzare un database per salvare le sessioni utente.
-// Alla fine di questo file (tra commenti) è possibile visualizzare una versione di questo codice senza sessioni.
-app.use(
-    session({
-      secret: 'SecretKey123',
-      resave: false,
-      saveUninitialized: true,
-    })
-);
+app.use(session({
+    secret: 'casual-secret-key-123456',
+    resave: false,
+    saveUninitialized: false
+}));
 
+// Passport.js middlewares for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport.js local configuration
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done) => {
+    try {
+        const user = await User.findOne({ email: email });
+        // If user not found show error message
+        if (!user) {
+            return done(null, false, { message: 'User not found.' });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        // If password is incorrect show error message
+        if (!validPassword) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+// Passport.js user serialization and deserialization (save user id in session)
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+});
+
+// Connect to database
+connectDB();
+
+// Database schemas
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+});
+
+const todoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+// Password hashing
+userSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+const TodoHome = mongoose.model('TodoHome', todoSchema);
+const TodoWork = mongoose.model('TodoWork', todoSchema);
+
+// Start page
 app.get('/', (req, res) => {
-    if (!req.session.todosPrinc) {
-        req.session.todosPrinc = [];
+    res.render('index.ejs');
+});
+
+// Login page
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+// Register page
+app.get('/register', (req, res) => {
+    res.render('signup.ejs');
+});
+
+// Register a new user
+app.post('/register', async (req, res) => {
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await User.findOne({ email: newUser.email });
+        // Check if user already exists
+        if (savedUser) {
+            req.flash('error', 'User already registered.');
+            res.redirect('/register');
+        } else {
+            await newUser.save();
+            // Immediately login user after registration
+            req.login(newUser, (err) => {
+                if (err) {
+                    req.flash('error', err.message);
+                    res.redirect('/register');
+                }
+                return res.redirect('/home');
+            });
+        }
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
     }
-    res.render('index.ejs', { todosPrinc: req.session.todosPrinc });
 });
 
-app.get('/work', (req, res) => {
-    if (!req.session.todosWork) {
-        req.session.todosWork = [];
+// Login a user
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+// Home page
+app.get('/home', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const userTodos = await TodoHome.find({ userId: req.user._id });
+        res.render('home.ejs', { user: req.user, todos: userTodos });
+    } else {
+        res.redirect('/login');
     }
-    res.render('work.ejs', { todosWork: req.session.todosWork });
 });
 
-app.post('/create', (req, res) => {
-    const todoPrinc = req.body.todoPrinc;
-    if (!req.session.todosPrinc) {
-        req.session.todosPrinc = [];
+// Work page
+app.get('/work', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const userTodos = await TodoWork.find({ userId: req.user._id });
+        res.render('work.ejs', { user: req.user, todos: userTodos });
+    } else {
+        res.redirect('/login');
     }
-    req.session.todosPrinc.push(todoPrinc);
-    res.redirect('/');
 });
 
-app.post('/work/create', (req, res) => {
-    const todoWork = req.body.todoWork;
-    if (!req.session.todosWork) {
-        req.session.todosWork = [];
+// Create a new todo - home
+app.post('/home/create', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const newTodo = new TodoHome({
+            text: req.body.todo,
+            userId: req.user._id
+        });
+    
+        try {
+            await newTodo.save();
+            res.redirect('/home');
+        } catch (error) {
+            res.render('error.ejs', { error: error.message });
+        }
+    } else {
+        res.redirect('/login');
     }
-    req.session.todosWork.push(todoWork);
-    res.redirect('/work');
 });
 
-app.get('/delete-all', (req, res) => {
-    req.session.todosPrinc = [];
-    res.redirect('/');
+// Create a new todo - work
+app.post('/work/create', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const newTodo = new TodoWork({
+            text: req.body.todo,
+            userId: req.user._id
+        });
+    
+        try {
+            await newTodo.save();
+            res.redirect('/work');
+        } catch (error) {
+            res.render('error.ejs', { error: error.message });
+        }
+    } else {
+        res.redirect('/login');
+    }
 });
 
-app.get('/work/delete-all', (req, res) => {
-    req.session.todosWork = [];
-    res.redirect('/work');
+// Delete a todo - home
+app.post('/home/delete', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const todoDelete = req.body.id;
+
+        try {
+            await TodoHome.findByIdAndDelete(todoDelete);
+            res.redirect('/home');
+        } catch (error) {
+            req.flash('error', error.message);
+        }
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+// Delete a todo - work
+app.post('/work/delete', async (req, res) => {
+    if (req.isAuthenticated()) {
+        const todoDelete = req.body.id;
+
+        try {
+            await TodoWork.findByIdAndDelete(todoDelete);
+            res.redirect('/work');
+        } catch (error) {
+            req.flash('error', error.message);
+        }
+    }
 });
 
-
-// --------------------------------------------------------------------------------------------------------------------
-
-/* import express from 'express';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-let todosPrinc = [];
-let todosWork = [];
-
-app.get('/', (req, res) => {
-    res.render('index.ejs', { todosPrinc: todosPrinc });
+// Delete all todos - home
+app.post('/home/deleteAll', async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            await TodoHome.deleteMany({ userId: req.user._id });
+            res.redirect('/home');
+        } catch (error) {
+            req.flash('error', error.message);
+        }
+    }
 });
 
-app.get('/work', (req, res) => {
-    res.render('work.ejs', { todosWork: todosWork });
+// Delete all todos - work
+app.post('/work/deleteAll', async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            await TodoWork.deleteMany({ userId: req.user._id });
+            res.redirect('/work');
+        } catch (error) {
+            req.flash('error', error.message);
+        }
+    }
 });
 
-app.post('/create', (req, res) => {
-    todosPrinc.push(req.body.todoPrinc);
-    res.redirect('/');
+// Logout a user
+app.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return res.redirect('/');
+        }
+        res.redirect('/');
+    });
 });
 
-app.post('/work/create', (req, res) => {
-    todosWork.push(req.body.todoWork);
-    res.redirect('/work');
+// Listen to port
+app.listen(port, '192.168.1.33', () => {
+    console.log(`Example app listening at port: ${port}`);
 });
 
-app.get('/delete-all', (req, res) => {
-    todosPrinc = [];
-    res.redirect('/');
-});
-
-app.get('/work/delete-all', (req, res) => {
-    todosWork = [];
-    res.redirect('/work');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-}); */
+async function connectDB() {
+    try {
+        await mongoose.connect(URI, { useNewUrlParser: true });
+        console.log('Database connected!');
+    } catch (error) {
+        console.log(error);
+    }
+}
